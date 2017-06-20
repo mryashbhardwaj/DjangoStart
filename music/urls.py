3ee1c130e001ace4b05fce20a4c65e5f420a8bc6
root <imyashbhardwaj@gmail.com>
2017-06-20 08:01:41 +0530

    some changes made

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import  url
 from . import views
 
 urlpatterns = [
+	# /music/
     url(r'^$', views.index, name = 'index'),
+    # /music/14/
+    url(r'^(?P<album_id>[0-9]+)/$' , views.detail , name = 'detail'),
 ] 
--- music/views.py
+++ music/views.py
@@ -1,4 +1,14 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album
+from django.template import loader
 def index (request):
-	return HttpResponse("<h1>this is the music homepage</h1>")
+	all_albums= Album.objects.all()
+	template = loader.get_template("music/index.html")
+	context ={
+		"albums": all_albums ,
+	}
+	return HttpResponse(template.render(context,request))
+
+def detail(request, album_id):
+	return HttpResponse("<h2>Details for album id" + str(album_id) + "</h2>")
